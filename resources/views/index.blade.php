b71e030434ac43729760665372b74d21465d56a8
deknaa <kdkrisnaaa@gmail.com>
2024-06-12 14:20:21 +0800

    install livewire

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
   @vite('resources/js/app.js')
+  @livewireStyles
 </head>
 <body>
   <h1 class="text-7xl font-bold underline">
     Hello world!
   </h1>
+  @livewireScripts
 </body>
 </html>
