021b3f4d28602b9ac54095e78e91c3e25d285f86
deknaa <kdkrisnaaa@gmail.com>
2024-06-16 09:44:03 +0800

    install jetstream

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "node_modules/preline/dist/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("preline/plugin"),
-  ],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, typography],
+};
